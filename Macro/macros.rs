cc83bb253ce0d361220b09532960090a76980588
Leo20031999 <43154655+Leo20031999@users.noreply.github.com>
2020-11-16 16:12:32 -0300

    Update macros.rs

--- Macro/macros.rs
+++ Macro/macros.rs
@@ -1,18 +1,20 @@
 use std::collections::BTreeMap;
 use std::collections::HashMap;
-macro_rules! create{
-	($typ: ty; $($key: expr => $value: expr),*) => {
+macro_rules! create{// declaração da macro
+	//o $ indica as variáveis que serão utilizadas na macro. O ty depois dos : é o ty e expr indica que pode ser uma expressão
+	($typ: ty; $($key: expr => $value: expr),*) => { 
 		{
-			let mut s = <$typ>::new();
-			$(s.insert($key, $value);)*
+			let mut s = <$typ>::new();//Aqui um novo tipo será criado
+			$(s.insert($key, $value);)* //insert é uma função dentro das classes BTreeMap e HashMap
 			s
 		}
 	};
 }
 
 fn main(){
-	let map = create!(HashMap<&str, i32>; "Lucas" => 12, "Leonardo" => 21, "Marcos" => 24, "Ana" => 10);
+	//Aqui o tipo é BTreeMap, mas pode ser HashMap ou outro tipo de classe ou struct que possua uma função insert semelhante a que eu usei.
+	let map = create!(BTreeMap<&str, i32>; "Lucas" => 12, "Leonardo" => 21, "Marcos" => 24, "Ana" => 10);
 	for(key,values) in &map{
-		println!("Name: {} -> Age: {}", key, values);  
+		println!("Name: {} -> Age: {}", key, values);
 	}
 }
