633a30be3dd2687838fcf04269c039ff4764bcda
Leo20031999 <43154655+Leo20031999@users.noreply.github.com>
2020-11-16 16:01:15 -0300

    Update macros.rs

--- macros.rs
+++ macros.rs
@@ -1,7 +1,9 @@
 use std::collections::BTreeMap;
 use std::collections::HashMap;
-macro_rules! create{
-	($typ: ty; $($key: expr => $value: expr),*) => {
+macro_rules! create{//declaração da macro
+	//$ é uma daclaração de uma variável que será usada na macro
+	//o ty seguido pelos : indica um tipo, o expr indica uma expressão
+	($typ: ty; $($key: expr => $value: expr),*) => {// a => indica o início do bloco de instruções
 		{
 			let mut s = <$typ>::new();
 			$(s.insert($key, $value);)*
@@ -11,7 +13,7 @@ macro_rules! create{
 }
 
 fn main(){
-	let map = create!(BTreeMap<&str, i32>; "Lucas" => 12, "Leonardo" => 21, "Marcos" => 24, "Ana" => 10);
+	let map = create!(BTreeMap<&str, i32>; "Lucas" => 12, "Leonardo" => 21, "Marcos" => 24, "Ana" => 10);// chamada da macro na main
 	for(key,values) in &map{
 		println!("Name: {} -> Age: {}", key, values);  
 	}
