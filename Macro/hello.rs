1ef1443e681af5af13191c1f777af46c2e6ff07f
Leo20031999 <43154655+Leo20031999@users.noreply.github.com>
2020-11-16 16:07:20 -0300

    Update hello.rs

--- Macro/hello.rs
+++ Macro/hello.rs
@@ -1,9 +1,9 @@
-macro_rules! hello{
-	()=>{
+macro_rules! hello{//declaração da macro
+	()=>{	   //começo do bloco de instruções
 			print!("Hello World\n");
 		}
 }
 
 fn main(){
-	hello!();
+	hello!();//chamada da macro na main
 }
