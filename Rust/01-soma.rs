2f130bdd46650fe621be38b03a146bdb65b7bca2
CaioSantana123 <61979486+CaioSantana123@users.noreply.github.com>
2020-11-16 15:36:35 -0300

    Update and rename soma.rs to 01-soma.rs

--- Rust/01-soma.rs
+++ Rust/01-soma.rs
@@ -2,11 +2,11 @@ use std::io;
 
 fn main(){
 	let n1 : u32;
-	let n2 : u32;
-	let mut input = String::new();
+	let n2 : u32;				//Declarando duas variáveis imutáveis
+	let mut input = String::new();		//String mutável que servirá de auxilio para entrada e saída
 	match io::stdin().read_line(&mut input){
 		Ok(__) => {},
-		Err(_e) => {println!("Erro na leitura de dados\n")},
+		Err(_e) => {println!("Erro na leitura de dados\n")},      //Verificando caso haja algum erro na leitura
 											}
 	n1 = input.trim().parse::<u32>().unwrap();
 	input = String::new();
