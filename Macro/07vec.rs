c38edd6b90010cb763a0e301e35e86c6d1355aa9
Leo20031999 <43154655+Leo20031999@users.noreply.github.com>
2020-11-22 17:56:26 -0300

    Update 07vec.rs

--- Macro/07vec.rs
+++ Macro/07vec.rs
@@ -1,14 +1,14 @@
 macro_rules! tvec{
-	($($x:expr),*) => {
+	($($x:expr),*) => {//criação da macro tvec
 		{
-			let mut t_vec = Vec::new();
-			$(t_vec.push($x);)*
-			t_vec
+			let mut t_vec = Vec::new();//criação do objeto Vec dentro da macro
+			$(t_vec.push($x);)*  //repetição da instrução push(função dentro da classe Vec)
+			t_vec  //retorna t_vec
 		}
 	};
 }
 
 fn main(){
-	let v = tvec!(1, 2, 3, 4, 5);
+	let v = tvec!(1, 2, 3, 4, 5); //chamada da macro
 	print!("Vetor criado: {:?}", v);
 }
