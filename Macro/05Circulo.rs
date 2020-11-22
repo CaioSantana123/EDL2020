6935532b1d27fa03e8e7872627c7d4abaa9777ce
Leo20031999 <43154655+Leo20031999@users.noreply.github.com>
2020-11-22 17:54:04 -0300

    Update 05Circulo.rs

--- Macro/05Circulo.rs
+++ Macro/05Circulo.rs
@@ -1,21 +1,21 @@
-pub struct Circulo{
+pub struct Circulo{//criação da estrutura círculo
 	raio : f32,
 }
 
 impl Circulo{
-	pub fn new(raio: f32) -> Circulo{
+	pub fn new(raio: f32) -> Circulo{//construtor
 		Circulo{
 			raio: raio,
 		}
 	}
-	pub fn calcula_raio(&self) -> f32{
+	pub fn calcula_raio(&self) -> f32{//cálculo do raio
 		return 2.0 * self.raio * 3.14;
 	}
 }
 
 
 fn main(){
-	let c = Circulo::new(4.2);
+	let c = Circulo::new(4.2);//criação do objeto
 	print!("Area: {}", c.calcula_raio());
 }
 
